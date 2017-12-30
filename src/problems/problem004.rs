1c1656679959a619464ab2bc10760345899bf13d
Jort de Nooij <jort.de.nooij@protonmail.com>
2017-12-30 21:01:18 +0100

    Optimized problem 4

--- src/problems/problem004.rs
+++ src/problems/problem004.rs
@@ -1,7 +1,7 @@
-pub fn largest_palindrome_product(max_product: i32) -> i32{
+pub fn largest_palindrome_product(n: u32) -> i32{
     let mut largest_palindrome_product: i32 = 0;
-    for i in 1..max_product {
-        for j in 1..max_product {
+    for i in 10_i32.pow(n-1)..10_i32.pow(n) {
+        for j in 10_i32.pow(n-1)..10_i32.pow(n) {
             let product = i * j;
             let string = product.to_string();
             if string.bytes().eq(string.bytes().rev()) {
@@ -15,5 +15,5 @@ pub fn largest_palindrome_product(max_product: i32) -> i32{
 }
 #[test]
 fn test_largest_palindrome_product(){
-    assert_eq!(largest_palindrome_product(100), 9009);
+    assert_eq!(largest_palindrome_product(2), 9009);
 }
