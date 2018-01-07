d819d70456245809e1f5c14876ee570e3f02ff59
Jort de Nooij <jort.de.nooij@protonmail.com>
2018-01-07 19:23:54 +0100

    Created problem 8 and 9.

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use problems::problem004;
 use problems::problem005;
 use problems::problem006;
 use problems::problem007;
+use problems::problem008;
+use problems::problem009;
 
 fn main() {
     println!("1: sum of all the multiples of 3 or 5 below 1000 = {0}", problem001::sum_of_multiples(3, 5, 1000));
@@ -16,4 +18,6 @@ fn main() {
     println!("5: smallest positive number that is evenly dividable by all of the numbers from 1 to 20 = {}", problem005::smallest_multiple(20));
     println!("6: difference between the sum of the squares of the first one hundred natural numbers and the square of the sum = {}", problem006::sum_square_difference(100));
     println!("7: the 10001th prime number = {}", problem007::nth_prime_number(10001));
+    println!("8: the largest product of thirteen adjacent digits in the 1000-digit number = {}", problem008::largest_product_in_a_series(13));
+    println!("9: The product of Pythagorean triplet for which a + b + c = 1000, = {}", problem009::find_pythagorean_triplet(1000));
 }
---
+++ src/problems/problem009.rs
@@ -0,0 +1,17 @@
+pub fn find_pythagorean_triplet(sum: i32) -> i32{
+    let mut a = 1;
+    let mut b = sum - 1;
+    loop {
+        let c = sum - b - a;
+        let c_squared = c*c;
+        let c2 = a*a + b*b;
+        if c2 == c_squared {
+            println!("a = {}, b = {}, c = {}", a, b, c);
+            return a * b * c;
+        }else if c2 > c_squared {
+            b -= 1;
+        } else {
+            a += 1;
+        }
+    }
+}
