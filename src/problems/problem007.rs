fb9178e5c056b67270549fc4da62e811b1311db8
Jort de Nooij <jort.de.nooij@protonmail.com>
2018-01-07 15:53:43 +0100

    Improved problem 7.

--- src/problems/problem007.rs
+++ src/problems/problem007.rs
@@ -1,11 +1,11 @@
-pub fn nth_prime_number(n: i32) -> i32{
-    let mut prime_numbers: Vec<i32> = Vec::new();
-    let mut prime = 2;
-    let mut count = 1;
+pub fn nth_prime_number(n: usize) -> i32{
+    let mut prime_numbers: Vec<i32> = Vec::with_capacity(n - 1);
+    let mut prime = 3;
+    let mut count = 2;
     'prime_test: loop {
         for i in prime_numbers.iter() {
             if prime % i == 0 {
-                prime += 1;
+                prime += 2;
                 continue 'prime_test;
             }
         }
